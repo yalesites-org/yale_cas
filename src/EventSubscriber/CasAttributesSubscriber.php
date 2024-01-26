ab66f489a823922ad818986c2e65a11edcb79a77
David Blankenship <david.blankenship@yale.edu>
2024-01-26 17:12:10 -0500

    YALB-1701: append app=yalesites to cas login
    On initial redirect, inject a query string for app=yalesites

--- src/EventSubscriber/CasAttributesSubscriber.php
+++ src/EventSubscriber/CasAttributesSubscriber.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\yale_cas\EventSubscriber;
 
+use Drupal\cas\Event\CasPreRedirectEvent;
+use Drupal\cas\Service\CasHelper;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Modifies the route system.
@@ -15,24 +15,18 @@ class CasAttributesSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = ['onKernelRequestCheckCustomParam'];
+    $events = [];
+    $events[CasHelper::EVENT_PRE_REDIRECT][] = ['onCasPreRedirect'];
+
     return $events;
   }
 
   /**
-   * Checks if the request is on the front page and has a custom parameter.
    *
-   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
-   *   The event to process.
    */
-  public function onKernelRequestCheckCustomParam(RequestEvent $event) {
-    // Yale CAS service owners will send CAS attributes back to the website
-    // with the query string "?app=yalesites". This event subscriber will direct
-    // these requests to the normal CAS service controller.
-    $request = $event->getRequest();
-    if ($request->query->has('app') && $request->query->get('app') == 'yalesites') {
-      $request->attributes->set('_controller', '\Drupal\cas\Controller\ServiceController::handle');
-    }
+  public function onCasPreRedirect(CasPreRedirectEvent $event) {
+    $casRedirectData = $event->getCasRedirectData();
+    $casRedirectData->setServiceParameter('app', 'yalesites');
   }
 
 }
